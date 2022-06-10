6936820bb44d163512efb77f6d0e87366ca4dbe6
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2022-06-09 23:45:24 -0700

    Made the pseudo code and the basic background for the game

---
+++ Roguh js.js
@@ -0,0 +1,10 @@
+//playing the card
+//activating the card
+//discard pile
+//when ending your turn
+//guess a card with the "guard" ability
+//comparing the card without reveling the card to another
+//score tracker
+//when deck of carcs runs out, then compare card from the remaining playes
+//discarding the "princess card" effect
+//win con is met 
